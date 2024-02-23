b0b24a94ce0edcbe13c6ad27745bfefc5d53b08a
totoro <bigchun@hanmail.net>
2024-02-23 11:11:30 +0900

    1

---
+++ src/main/java/com/dream/VueBootBoardApplication.java
@@ -0,0 +1,13 @@
+package com.dream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueBootBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueBootBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dream/controller/VueBootBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dream.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueBootBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
